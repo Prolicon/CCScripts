37a745af3f8ceed295cc40af27b7ffb72d56ebbe
Prolicon <kiritotheillumatidorito@gmail.com>
2022-12-20 03:32:05 +0000

    More

--- Pocket/colony.lua
+++ Pocket/colony.lua
@@ -1,7 +1,7 @@
-HCHANNEL = 150
-RCHANNEL = 151
+local HCHANNEL = 150
+local RCHANNEL = 151
 
-modem = peripheral.find("modem")
+local modem = peripheral.find("modem")
 assert(modem, "Modem not attached")
 assert(modem.isWireless, "Modem is not wireless")
 
@@ -37,20 +37,55 @@ function getBuilderResources(position)
     return data
 end
 
-function drawBuilder(y, name, status, selected)
-    y = y*3-1
+function drawBuilder(n, selected)
+    y = n*3-1
     local pre = selected and ARROWRIGHT or " "
     local post = selected and ARROWLEFT or " "
     term.setBackgroundColor(selected and colors.lime or colors.green)
     term.setTextColor(colors.gray)
     term.setCursorPos(2,y)
-    term.write(string.format(pre.." %-21s"..post, name))
+    term.write(string.format(pre.." %-21s"..post, builders[n].name))
     term.setCursorPos(2,y+1)
-    term.write(string.format(pre.."%21s "..post, "STATUS"))
+    term.write(string.format(pre.."%21s "..post, builders[n].status))
 end
 
-drawBuilder(1, "Bobert B. Brown", "SUPPLY", false)
-drawBuilder(2, "Nib G. Gur", "IDLE", true)
-drawBuilder(3, "Octavia C. Cutter", "GOOD", false)
+local bPage = {pos = 1}
 
-os.pullEvent("char")
+function bPage.input(key)
+    if key == keys.w or key == keys.up then
+        drawBuilder(bPage.pos, false)
+        bPage.pos = bPage.pos >= #builders and 1 or bPage.pos + 1
+        drawBuilder(bPage.pos, "IDLE", true)
+    elseif key == keys.s or key == keys.down then
+        drawBuilder(bPage.pos, false)
+        bPage.pos = bPage.pos <= 1 and #builders or bPage.pos - 1
+        drawBuilder(bPage.pos, true)
+    end
+end
+
+function bPage.render()
+    term.clear()
+    for pos, builder in ipairs(builder) do
+        drawBuilder(pos, pos == bPage.pos)
+    end
+end
+
+local builders = {}
+builders[1] = {name = "Bobert B. Brown", status = "idle"}
+builders[2] = {name = "Nib G. Gur", status = "good"}
+builders[3] = {name = "Octavia C. Cutter", status = "supply"}
+
+currentPage = bPage
+currentPage.render()
+
+while true do
+    _, key, isHeld = os.pullEvent("key")
+    if key == keys.backspace then break end
+    currentPage.input(key)
+end
+
+-- Reset on exit
+term.setBackgroundColor(colors.black)
+term.setTextColor(colors.white)
+term.setCursorPos(1,1)
+term.clear()
